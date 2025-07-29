d5f99ac5bfe837d71f4e30ad8f6cdbd2cdc0b106
ali-mansurr <197986189+ali-mansurr@users.noreply.github.com>
2025-07-29 20:54:28 +0330

    Implement grid resizing functionality

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const controlBtns = document.querySelector(".buttons");
 const gridContainer = document.querySelector(".grid-container");
+let gridSize = parseInt(prompt("Enter grid size (if grid is n then it will look like n * n)"))
 function generateGrid(grid) {
-    if (grid > 100) return "max grid : 100*100";
+    if (grid > 100 || grid < 0 || !grid ) {
+        alert("please enter a valid number between 1 to 100");
+        return;
+    };
     const containerSize = gridContainer.clientWidth;
     const tileSize = containerSize / grid;
     for (let i = 1; i <= grid * grid; i++) {
@@ -19,15 +23,28 @@ function draw(event) {
 }
 
 function erase(event) {
-    if(!event.target.matches("button")) return;
+    if (!event.target.matches("button")) return;
     if (event.target.classList.contains("erase")) {
-        for(const tile of gridContainer.children){
+        for (const tile of gridContainer.children) {
             tile.classList.remove("draw");
         }
     }
 }
 
-generateGrid(16);
+function changeGridSize(event) {
+    if (!event.target.matches("button")) return;
+    if (event.target.classList.contains("resize")) {
+        gridSize = parseInt(prompt("Enter grid size (if grid is n then it will look like n * n)"))
+        for (const child of Array.from(gridContainer.children)) { //convert to static array because of live collection issue
+            child.remove();
+        }
+        generateGrid(gridSize);
+    }
+}
+
+
+generateGrid(gridSize);
+console.log(typeof(gridSize))
 
 // draw on hover
 gridContainer.addEventListener("mouseover", draw)
@@ -35,4 +52,5 @@ gridContainer.addEventListener("mouseover", draw)
 // erase 
 controlBtns.addEventListener("click", (e) => {
     erase(e)
+    changeGridSize(e)
 })
