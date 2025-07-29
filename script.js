70bc53c6501868db83289e9bf721792392b1e3d0
ali-mansurr <197986189+ali-mansurr@users.noreply.github.com>
2025-07-29 19:44:16 +0330

    Calculate and set grid tile dimensions

--- script.js
+++ script.js
@@ -3,9 +3,13 @@ const gridContainer = document.querySelector(".grid-container");
 
 function generateGrid(grid) {
     if (grid > 100) return "max grid : 100*100";
+    const containerSize = gridContainer.clientWidth;
+    const tileSize = containerSize / grid;
     for (let i = 1; i <= grid * grid; i++) {
         const div = document.createElement("div");
         div.classList.add("tile");
+        div.style.width = `${tileSize}px`;
+        div.style.height = `${tileSize}px`;
         gridContainer.appendChild(div);
     }
 }
